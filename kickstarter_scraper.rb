78b7480b74575cfd9c08a34e24f7373661bd43cb
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-05 03:07:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,9 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  project = {}
+  
 end
 
 create_project_hash
